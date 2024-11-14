993a39e3fd768c662e547ef166c9e09ddfd5e3bb
seongjxn <seongjxn@ivyforest.kr>
2024-11-14 15:47:04 +0900

    [UPDATE] Navigation 페이지 별 별도 파일 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'src/map.dart';
+import 'src/search.dart';
+import 'src/menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,59 +10,58 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        fontFamily: 'KoddiUDOnGothic',
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'bottomNavigationBar Test'),
+        home: const HomeScreen(),
+        theme: ThemeData(
+          fontFamily: 'KoddiiUDOnGothic',
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+          useMaterial3: true,
+        )
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final List<Widget> _screens = [
+    const MapScreen(),
+    const SearchScreen(),
+    const MenuScreen(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      body: _screens[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
-        items: [
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
           BottomNavigationBarItem(
-              icon: Icon(Icons.map),
-              label: '지도'
+            icon: Icon(Icons.map),
+            label: '지도',
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: '검색'
+            icon: Icon(Icons.search),
+            label: '검색',
           ),
           BottomNavigationBarItem(
-              icon: Icon(Icons.menu),
-              label: '메뉴'
+            icon: Icon(Icons.menu),
+            label: '메뉴',
           ),
         ],
       ),
