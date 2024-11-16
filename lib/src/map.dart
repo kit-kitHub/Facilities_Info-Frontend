a5ec7e988bdb9a2a9f24aae447c6b914c004be75
KangJiHyeock <rkdwlgur020297@gmail.com>
2024-11-16 21:36:03 +0900

    [Update] 현재 위치 아이콘 수정, 현재 위치 수정
    현재 위치버튼을 누르면 현재 위치로 계속 이동(5m간격으로)하도록 수정,
    현재 위치가 이동하면 마커도 따라가도록 생성
    현재 위치 아이콘 수정 및 on off 버튼 형식으로 제작

--- lib/src/map.dart
+++ lib/src/map.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 import 'package:geolocator/geolocator.dart';
@@ -15,28 +16,52 @@ class _MainScreenState extends State<MapScreen> {
   Set<Marker> markers = {}; // Marker variable
   String? latitude;
   String? longitude;
+  bool isTracking = false;  // 추적 상태를 나타내는 변수
+  StreamSubscription<Position>? positionStream;
 
-  //현재 위치 정보를 가져와주는 함수
-  getGeoData() async {
+  //현재 위치를 움직이면 마커가 따라오게 해주는 함수
+  void startTracking() async {
     LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        return Future.error('permissions are denied');
+        return Future.error('Location permissions are denied');
       }
     }
-    //geolocator을 이용하여 현재 위치정보를 가져와 latitude(위도)와 longitude(경도)에 넣어준다.
-    Position position = await Geolocator.getCurrentPosition();
-    setState(() {
-      latitude = position.latitude.toString();
-      longitude= position.longitude.toString();
+
+    positionStream = Geolocator.getPositionStream(
+      locationSettings: const LocationSettings(
+        accuracy: LocationAccuracy.high,
+        distanceFilter: 5, // 최소 이동 거리 (5미터)
+      ),
+    ).listen((Position position) {
+      LatLng currentPosition = LatLng(position.latitude, position.longitude);
+
+      // 지도 중심과 마커를 업데이트
+      updateMarker(currentPosition, '현재 위치');
     });
   }
 
-  @override
-  void initState() {
-    super.initState();
-    getGeoData();
+  // 위치 추적을 중지하는 함수
+  void stopTracking() {
+    positionStream?.cancel();  // 위치 추적 취소
+  }
+
+  //마커 띄울때 사용하는 함수
+  void updateMarker(LatLng position, String infoText) {
+    setState(() {
+      markers.clear();
+      markers.add(
+        Marker(
+          markerId: UniqueKey().toString(),
+          latLng: position,
+          infoWindowContent: infoText,    //위에 infoText로 넘어온 것들 마커 위에 표시해준다
+          infoWindowFirstShow: false,
+          infoWindowRemovable: true,
+        ),
+      );
+    });
+    mapController.setCenter(position);
   }
 
   @override
@@ -48,16 +73,18 @@ class _MainScreenState extends State<MapScreen> {
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
             child: KakaoMap(
-              onMapCreated: ((controller) async {
-                mapController = controller;
-                markers.add(Marker(
-                  markerId: UniqueKey().toString(),
-                  latLng: await mapController.getCenter(),
-                ));
-                setState(() {});
+              onMapTap: ((controller) async {
+                mapController.clear();
               }),
+              onMapCreated: (controller) async {
+                mapController = controller;
+                LatLng initialPosition = LatLng(37.3608681, 126.9306506);
+                updateMarker(initialPosition, 'Test');
+              },
               markers: markers.toList(),
               center: LatLng(37.3608681, 126.9306506),
+              onMarkerTap: (String markerId, LatLng position, int index) async{
+              },
             ),
           ),
           Positioned(
@@ -73,24 +100,23 @@ class _MainScreenState extends State<MapScreen> {
                   child: Icon(Icons.add),
                 ),
                 SizedBox(height: 20),
+
                 FloatingActionButton(
-                  onPressed: () async {
-                    Position position = await Geolocator.getCurrentPosition();            //geolocator을 이용한 현재 위치 정보 가져오기
-                    LatLng userLocation = LatLng(position.latitude, position.longitude);
-
-                    mapController.setCenter(userLocation);
-
+                  onPressed: () {
                     setState(() {
-                      markers.clear();                  //이전에 생긴 마커들은 제거
-                      markers.add(Marker(               //현재 위치에 마커 생성
-                        markerId: UniqueKey().toString(),
-                        latLng: userLocation,
-                      ));
-                      onCameraIdle: (userLocation, 3);  //마커 생성된 현재 위치로 카메라 이동
+                      if (isTracking) {
+                        stopTracking();  // 이미 추적 중이면 멈추기
+                      } else {
+                        startTracking();  // 추적 시작
+                      }
+                      isTracking = !isTracking;  // 추적 상태 토글
                     });
                   },
-                  child: Icon(Icons.location_on),
+                  child: Icon(
+                    isTracking ? Icons.my_location_rounded : Icons.my_location,  // 상태에 따라 아이콘 변경
+                  ),
                 ),
+
                 SizedBox(height: 20),
                 FloatingActionButton(
                   onPressed: () {
@@ -99,7 +125,6 @@ class _MainScreenState extends State<MapScreen> {
                     });
                   },
                   child: Icon(Icons.refresh),
-                  heroTag: 'refresh',
                 ),
               ],
             ),
