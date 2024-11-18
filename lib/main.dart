2ba5c81e627b6c2af536e5b26fe5f48e6c9e52dc
KangJiHyeock <rkdwlgur020297@gmail.com>
2024-11-18 23:30:54 +0900

    [Update] Search 폰트 적용

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class _HomeScreenState extends State<HomeScreen> {
 
   final List<Widget> _screens = [
     const MapScreen() ,
-    SearchScreen(),
+    const SearchScreen(),
     const MenuScreen(),
   ];
 
