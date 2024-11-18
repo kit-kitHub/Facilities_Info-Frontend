8b01d2aeab6bd48c45ca7793d68329cb5315c203
KangJiHyeock <rkdwlgur020297@gmail.com>
2024-11-19 00:58:59 +0900

    [Update] map.dart - 지도 현재 위치 저장 성공

--- lib/src/map.dart
+++ lib/src/map.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 import 'package:geolocator/geolocator.dart';
+import '/SingleTone/map_center.dart';
 
 
 class MapScreen extends StatefulWidget {
@@ -18,6 +19,8 @@ class _MainScreenState extends State<MapScreen> {
   String? longitude;
   bool isTracking = false;  // 추적 상태를 나타내는 변수
   StreamSubscription<Position>? positionStream;
+  int level = 4;
+  final mapcentermanager = mapCenterManager(); //지도 현재 위치 저장용용
 
   //현재 위치를 움직이면 마커가 따라오게 해주는 함수
   void startTracking() async {
@@ -36,7 +39,8 @@ class _MainScreenState extends State<MapScreen> {
       ),
     ).listen((Position position) {
       LatLng currentPosition = LatLng(position.latitude, position.longitude);
-
+      mapcentermanager.mapCenterlatitude = position.latitude;
+      mapcentermanager.mapCenterlongitude = position.longitude;
       // 지도 중심과 마커를 업데이트
       updateMarker(currentPosition, '현재 위치');
     });
@@ -75,29 +79,26 @@ class _MainScreenState extends State<MapScreen> {
             child: KakaoMap(
               onMapCreated: (controller) async {
                 mapController = controller;
-                LatLng initialPosition = LatLng(37.3608681, 126.9306506);
+                LatLng initialPosition = LatLng(mapcentermanager.mapCenterlatitude, mapcentermanager.mapCenterlongitude);
                 updateMarker(initialPosition, 'Test');
               },
               markers: markers.toList(),
-              center: LatLng(37.3608681, 126.9306506),
+              center: LatLng(mapcentermanager.mapCenterlatitude, mapcentermanager.mapCenterlongitude),
               onMarkerTap: (String markerId, LatLng position, int index) async{
               },
             ),
           ),
           Positioned(
-            bottom: 20,
+            top: 40,
             right: 20,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
                 FloatingActionButton(
-                  onPressed: () async {
-                  },
-                  child: Icon(Icons.add),
-                ),
-                SizedBox(height: 20),
-
-                FloatingActionButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(90),
+                  ),
+                  backgroundColor: Colors.white,
                   onPressed: () {
                     setState(() {
                       if (isTracking) {
@@ -109,18 +110,52 @@ class _MainScreenState extends State<MapScreen> {
                     });
                   },
                   child: Icon(
-                    isTracking ? Icons.my_location_rounded : Icons.my_location,  // 상태에 따라 아이콘 변경
+                    Icons.my_location,
+                    color: isTracking ? Colors.blue : Colors.black87 , // 상태에 따라 색상 변경
                   ),
                 ),
-
                 SizedBox(height: 20),
                 FloatingActionButton(
-                  onPressed: () {
-                    setState(() {
-                      markers.clear();
-                    });
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(90),
+                  ),
+                  backgroundColor: Colors.white,
+                  onPressed: () async {
+                    level = level - 1;
+                    mapController.setLevel(level);
                   },
-                  child: Icon(Icons.refresh),
+                  child: Icon(Icons.add),
+                ),
+                SizedBox(height: 20),
+                FloatingActionButton(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(90),
+                  ),
+                  backgroundColor: Colors.white,
+                  onPressed: () async {
+                    level = level + 1;
+                    mapController.setLevel(level);
+                  },
+                  child: Icon(Icons.remove),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                SizedBox(
+                  child: FloatingActionButton.extended(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(90),
+                    ),
+                    backgroundColor : Colors.white,
+                    onPressed: () {},
+                    label: Text('추가하기', style: TextStyle(color: Colors.black)),
+                    ),
                 ),
               ],
             ),
