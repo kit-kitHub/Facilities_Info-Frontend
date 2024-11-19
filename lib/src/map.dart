b7b58275d59f0c2fdc73063b3079373ea48f16c2
KangJiHyeock <rkdwlgur020297@gmail.com>
2024-11-19 12:45:59 +0900

    Update map.dart

--- lib/src/map.dart
+++ lib/src/map.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 import 'package:geolocator/geolocator.dart';
 
+import '/SingleTone/map_center.dart';//화면 이동해도 화면 남아있게 하기위해 사용하는 싱글톤
+
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -18,6 +20,9 @@ class _MainScreenState extends State<MapScreen> {
   String? longitude;
   bool isTracking = false;  // 추적 상태를 나타내는 변수
   StreamSubscription<Position>? positionStream;
+  int level = 4;
+  bool isRunning = false;
+  final mapcentermanager = mapCenterManager();
 
   //현재 위치를 움직이면 마커가 따라오게 해주는 함수
   void startTracking() async {
@@ -32,13 +37,14 @@ class _MainScreenState extends State<MapScreen> {
     positionStream = Geolocator.getPositionStream(
       locationSettings: const LocationSettings(
         accuracy: LocationAccuracy.high,
-        distanceFilter: 5, // 최소 이동 거리 (5미터)
+        distanceFilter: 1, // 최소 이동 거리 (5미터)
       ),
     ).listen((Position position) {
       LatLng currentPosition = LatLng(position.latitude, position.longitude);
-
+      mapcentermanager.mapCenterlatitude = position.latitude;
+      mapcentermanager.mapCenterlongitude = position.longitude;
       // 지도 중심과 마커를 업데이트
-      updateMarker(currentPosition, '현재 위치');
+      updateMarker(currentPosition);
     });
   }
 
@@ -48,82 +54,207 @@ class _MainScreenState extends State<MapScreen> {
   }
 
   //마커 띄울때 사용하는 함수
-  void updateMarker(LatLng position, String infoText) {
+  void updateMarker(LatLng position) {
     setState(() {
       markers.clear();
       markers.add(
         Marker(
           markerId: UniqueKey().toString(),
           latLng: position,
-          infoWindowContent: infoText,    //위에 infoText로 넘어온 것들 마커 위에 표시해준다
-          infoWindowFirstShow: false,
-          infoWindowRemovable: true,
+        ),
+      );
+      mapController.setCenter(position);
+    });
+  }
+
+  //마커가 찍혀도 화면이 움직이지 않게 하기위한 함수
+  void updateMarker_not_center_move(LatLng position) {
+    setState(() {
+      markers.clear();
+      markers.add(
+        Marker(
+          markerId: UniqueKey().toString(),
+          latLng: position,
         ),
       );
     });
-    mapController.setCenter(position);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            child: KakaoMap(
-              onMapCreated: (controller) async {
-                mapController = controller;
-                LatLng initialPosition = LatLng(37.3608681, 126.9306506);
-                updateMarker(initialPosition, 'Test');
-              },
-              markers: markers.toList(),
-              center: LatLng(37.3608681, 126.9306506),
-              onMarkerTap: (String markerId, LatLng position, int index) async{
-              },
+        body:  SafeArea(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                child: KakaoMap(
+                  onMapCreated: (controller) async {
+                    mapController = controller;
+                    LatLng initialPosition = LatLng(mapcentermanager.mapCenterlatitude, mapcentermanager.mapCenterlongitude);
+                    updateMarker(initialPosition);
+                  },
+                  markers: markers.toList(),
+                  center: LatLng(mapcentermanager.mapCenterlatitude, mapcentermanager.mapCenterlongitude),
+                  onMarkerTap: (String markerId, LatLng position, int index) async{
+                  },
+                ),
+              ),
+              Positioned(
+                top: 40,
+                right: 20,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    FloatingActionButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(90),
+                      ),
+                      backgroundColor: Colors.white,
+                      onPressed: () {
+                        setState(() {
+                          if (isTracking) {
+                            stopTracking();  // 이미 추적 중이면 멈추기
+                          } else {
+                            startTracking();  // 추적 시작
+                          }
+                          isTracking = !isTracking;  // 추적 상태 토글
+                        });
+                      },
+                      child: Icon(
+                        Icons.my_location,
+                        color: isTracking ? Colors.blue : Colors.black87 , // 상태에 따라 색상 변경
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    FloatingActionButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(90),
+                      ),
+                      backgroundColor: Colors.white,
+                      onPressed: () async {
+                        level = level - 1;
+                        mapController.setLevel(level);
+                      },
+                      child: const Icon(Icons.add),
+                    ),
+                    const SizedBox(height: 20),
+                    FloatingActionButton(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(90),
+                      ),
+                      backgroundColor: Colors.white,
+                      onPressed: () async {
+                        level = level + 1;
+                        mapController.setLevel(level);
+                      },
+                      child: const Icon(Icons.remove),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: 20,
+                right: 20,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    SizedBox(
+                      child: FloatingActionButton.extended(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(90),
+                        ),
+                        backgroundColor : Colors.white,
+                        onPressed: () async {
+                          //추가하기 버튼이 이미 한번 눌렸던 상태였다면
+                          if (isRunning) {
+                            setState(() {
+                              isRunning = false;
+                              var moveLatLon = LatLng(mapcentermanager.mapCenterlatitude - 0.00125, mapcentermanager.mapCenterlongitude);
+                              mapController.panTo(moveLatLon);
+                              showModalBottomSheet(
+                                context: context,
+                                barrierColor: Colors.transparent,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                                ),
+                                builder: (context) {
+                                  return BottomSheetContent();
+                                },
+                              );
+                            });
+                          } 
+                          //아니면 무한반복으로 현재 화면 중간에 마커 생성
+                          else {
+                            setState(() {
+                              isRunning = true;
+                            });
+
+                            while (isRunning) {
+                              await Future.delayed(Duration(milliseconds: 10));
+                              LatLng center = await mapController.getCenter();
+                              mapcentermanager.mapCenterlongitude = center.longitude;
+                              mapcentermanager.mapCenterlatitude = center.latitude;
+                              updateMarker_not_center_move(center);
+                            }
+                          }
+                        },
+                        label: const Text('추가하기', style: TextStyle(color: Colors.black)),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
+
+//사용자가 정보를 작성하는 칸
+class BottomSheetContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      height: 400,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              labelText: '해당 위치 이름을 작성해 주세요',
+              border: OutlineInputBorder(),
             ),
           ),
-          Positioned(
-            bottom: 20,
-            right: 20,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                FloatingActionButton(
-                  onPressed: () async {
-                  },
-                  child: Icon(Icons.add),
-                ),
-                SizedBox(height: 20),
-
-                FloatingActionButton(
-                  onPressed: () {
-                    setState(() {
-                      if (isTracking) {
-                        stopTracking();  // 이미 추적 중이면 멈추기
-                      } else {
-                        startTracking();  // 추적 시작
-                      }
-                      isTracking = !isTracking;  // 추적 상태 토글
-                    });
-                  },
-                  child: Icon(
-                    isTracking ? Icons.my_location_rounded : Icons.my_location,  // 상태에 따라 아이콘 변경
-                  ),
-                ),
-
-                SizedBox(height: 20),
-                FloatingActionButton(
-                  onPressed: () {
-                    setState(() {
-                      markers.clear();
-                    });
-                  },
-                  child: Icon(Icons.refresh),
-                ),
-              ],
+          SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              // 이미지 추가 로직 구현
+            },
+            child: Text('이미지 추가하기'),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            maxLines: 5,
+            decoration: InputDecoration(
+              labelText: '내용을 적어주세요',
+              border: OutlineInputBorder(),
             ),
+          ),
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // 제출 로직 구현
+                },
+                child: Text('제출하기'),
+              ),
+            ],
           ),
         ],
       ),
