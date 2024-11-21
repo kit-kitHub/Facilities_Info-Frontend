a69f3e9128c3a450e62c5181625d4f07b555d8e3
KangJiHyeock <rkdwlgur020297@gmail.com>
2024-11-22 02:57:24 +0900

    Update map.dart 현재 위치 저장

--- lib/src/map.dart
+++ lib/src/map.dart
@@ -2,7 +2,10 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:kakao_map_plugin/kakao_map_plugin.dart';
 import 'package:geolocator/geolocator.dart';
-import '/SingleTone/map_center.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+
+import '/SingleTone/map_center.dart';//화면 이동해도 화면 남아있게 하기위해 사용하는 싱글톤
 
 
 class MapScreen extends StatefulWidget {
@@ -12,7 +15,7 @@ class MapScreen extends StatefulWidget {
   State<MapScreen> createState() => _MainScreenState();
 }
 
-class _MainScreenState extends State<MapScreen> {
+class _MainScreenState extends State<MapScreen> with WidgetsBindingObserver{
   late KakaoMapController mapController;
   Set<Marker> markers = {}; // Marker variable
   String? latitude;
@@ -20,6 +23,7 @@ class _MainScreenState extends State<MapScreen> {
   bool isTracking = false;  // 추적 상태를 나타내는 변수
   StreamSubscription<Position>? positionStream;
   int level = 4;
+  bool isRunning = false;
   final mapcentermanager = mapCenterManager();
 
   //현재 위치를 움직이면 마커가 따라오게 해주는 함수
@@ -35,14 +39,14 @@ class _MainScreenState extends State<MapScreen> {
     positionStream = Geolocator.getPositionStream(
       locationSettings: const LocationSettings(
         accuracy: LocationAccuracy.high,
-        distanceFilter: 1, // 최소 이동 거리 (1미터)
+        distanceFilter: 1, // 최소 이동 거리 (5미터)
       ),
     ).listen((Position position) {
       LatLng currentPosition = LatLng(position.latitude, position.longitude);
-      mapcentermanager.mapCenterlatitude = position.latitude;
-      mapcentermanager.mapCenterlongitude = position.longitude;
+      mapcentermanager.setMapCenterLongitude(position.longitude);
+      mapcentermanager.setMapCenterLatitude(position.latitude);
       // 지도 중심과 마커를 업데이트
-      updateMarker(currentPosition, '현재 위치');
+      updateMarker(currentPosition);
     });
   }
 
@@ -52,26 +56,80 @@ class _MainScreenState extends State<MapScreen> {
   }
 
   //마커 띄울때 사용하는 함수
-  void updateMarker(LatLng position, String infoText) {
+  void updateMarker(LatLng position) {
     setState(() {
       markers.clear();
       markers.add(
         Marker(
           markerId: UniqueKey().toString(),
           latLng: position,
-          infoWindowContent: infoText,    //위에 infoText로 넘어온 것들 마커 위에 표시해준다
-          infoWindowFirstShow: false,
-          infoWindowRemovable: true,
+        ),
+      );
+      mapController.setCenter(position);
+    });
+  }
+
+  //마커가 찍혀도 화면이 움직이지 않게 하기위한 함수
+  void updateMarker_not_center_move(LatLng position) {
+    setState(() {
+      markers.clear();
+      markers.add(
+        Marker(
+          markerId: UniqueKey().toString(),
+          latLng: position,
         ),
       );
     });
-    mapController.setCenter(position);
+  }
+
+  void SingleToneclear() async{
+    await mapCenterManager().initialize();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this); // AppLifecycleState 감지 활성화
+    mapCenterManager().initialize(); // 싱글톤 초기화
+
+    _restoreLastPosition(); // 마지막 위치 복원
+  }
+
+  Future<void> _restoreLastPosition() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    // SharedPreferences에서 저장된 위치 불러오기
+    double latitude = prefs.getDouble('last_latitude') ?? mapcentermanager.mapCenterlatitude;
+    double longitude = prefs.getDouble('last_longitude') ?? mapcentermanager.mapCenterlongitude;
+
+    // 지도 중심 위치 복원
+    LatLng lastPosition = LatLng(latitude, longitude);
+    mapController.setCenter(lastPosition);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    if (state == AppLifecycleState.paused || state == AppLifecycleState.detached) {
+      // 현재 지도 중심 좌표 가져오기
+      LatLng center = await mapController.getCenter();
+
+      // SharedPreferences에 저장
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setDouble('last_latitude', center.latitude);
+      await prefs.setDouble('last_longitude', center.longitude);
+    }
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this); // Observer 해제
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:  SafeArea(
+        body:  SafeArea(
           child: Stack(
             children: <Widget>[
               Container(
@@ -81,12 +139,34 @@ class _MainScreenState extends State<MapScreen> {
                   onMapCreated: (controller) async {
                     mapController = controller;
                     LatLng initialPosition = LatLng(mapcentermanager.mapCenterlatitude, mapcentermanager.mapCenterlongitude);
-                    updateMarker(initialPosition, 'Test');
+                    updateMarker(initialPosition);
                   },
+                  onZoomChangeCallback: (maplevel, context){
+                    level = maplevel;
+                  },
+                  zoomControl: false,
                   markers: markers.toList(),
                   center: LatLng(mapcentermanager.mapCenterlatitude, mapcentermanager.mapCenterlongitude),
                   onMarkerTap: (String markerId, LatLng position, int index) async{
-                  },
+                    showModalBottomSheet(
+                      context: context,
+                      backgroundColor: Colors.transparent,
+                      isScrollControlled: true, // 전체 화면 확장 가능
+                      builder: (BuildContext context) {
+                        return DraggableScrollableSheet(
+                          initialChildSize: 0.5, // 초기 크기: 화면의 절반
+                          minChildSize: 0.3, // 최소 크기
+                          maxChildSize: 0.9, // 최대 크기
+                          builder: (BuildContext context,
+                              ScrollController scrollController) {
+                            return DraggableSheet(
+                              scrollController: scrollController,
+                            );
+                          },
+                        );
+                      },
+                    );
+                    },
                 ),
               ),
               Positioned(
@@ -144,7 +224,7 @@ class _MainScreenState extends State<MapScreen> {
               ),
               Positioned(
                 bottom: 20,
-                right: 20,
+                left: 20,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
@@ -154,7 +234,37 @@ class _MainScreenState extends State<MapScreen> {
                           borderRadius: BorderRadius.circular(90),
                         ),
                         backgroundColor : Colors.white,
-                        onPressed: () {
+                        onPressed: () async {
+                          if (isRunning) {
+                            setState(() {
+                              isRunning = false;
+                              var moveLatLon = LatLng(mapcentermanager.mapCenterlatitude - 0.00125, mapcentermanager.mapCenterlongitude);
+                              mapController.panTo(moveLatLon);
+                              showModalBottomSheet(
+                                context: context,
+                                barrierColor: Colors.transparent,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                                ),
+                                builder: (context) {
+                                  return BottomSheetContent_find();
+                                },
+                              );
+                            });
+                          }
+                          else {
+                            setState(() {
+                              isRunning = true;
+                            });
+
+                            while (isRunning) {
+                              await Future.delayed(Duration(milliseconds: 10));
+                              LatLng center = await mapController.getCenter();
+                              mapcentermanager.setMapCenterLongitude(center.longitude);
+                              mapcentermanager.setMapCenterLatitude(center.latitude);
+                              updateMarker_not_center_move(center);
+                            }
+                          }
                         },
                         label: const Text('추가하기', style: TextStyle(color: Colors.black)),
                       ),
@@ -162,9 +272,171 @@ class _MainScreenState extends State<MapScreen> {
                   ],
                 ),
               ),
+          ]
+        ),
+      ),
+    );
+  }
+}
+
+//사용자가 정보를 작성하는 칸
+class BottomSheetContent_find extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      height: 400,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              labelText: '해당 위치 이름을 작성해 주세요',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              // 이미지 추가 로직 구현
+            },
+            child: Text('이미지 추가하기'),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            maxLines: 5,
+            decoration: InputDecoration(
+              labelText: '내용을 적어주세요',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // 제출 로직 구현
+                },
+                child: Text('제출하기'),
+              ),
             ],
           ),
-      )
+        ],
+      ),
+    );
+  }
+}
+
+//마커의 정보가 보여지는 칸
+class DraggableSheet extends StatelessWidget {
+  final ScrollController scrollController;
+
+  DraggableSheet({required this.scrollController});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(16),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.transparent,
+            blurRadius: 5,
+            offset: Offset(0, -3),
+          ),
+        ],
+      ),
+      child: ListView(
+        controller: scrollController,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '금오공과대학교 야외공연장',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8),
+                Text(
+                  '경상북도 구미시 거의동 472-1',
+                  style: TextStyle(color: Colors.grey[700]),
+                ),
+                SizedBox(height: 16),
+                Image.network(
+                  'https://via.placeholder.com/300', // 이미지 URL
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(height: 16),
+                Text(
+                  '사용자가 작성한 간단한 설명',
+                  style: TextStyle(fontSize: 16),
+                ),
+                SizedBox(height: 16),
+                Row(
+                  children: [
+                    Text(
+                      '5.0',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 8),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    SizedBox(width: 8),
+                    Text('리뷰 1개'),
+                  ],
+                ),
+                SizedBox(height: 16),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: '리뷰 작성하기',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('리뷰 제출하기'),
+                ),
+                SizedBox(height: 16),
+                Divider(),
+                ListTile(
+                  leading: CircleAvatar(),
+                  title: Text('사용자 이름1'),
+                  subtitle: Text('리뷰 내용'),
+                  trailing: Text('2024/11/06'),
+                ),
+                ListTile(
+                  leading: CircleAvatar(),
+                  title: Text('사용자 이름2'),
+                  subtitle: Text('리뷰 내용'),
+                  trailing: Text('2024/11/06'),
+                ),
+                ListTile(
+                  leading: CircleAvatar(),
+                  title: Text('사용자 이름3'),
+                  subtitle: Text('리뷰 내용'),
+                  trailing: Text('2024/11/06'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
