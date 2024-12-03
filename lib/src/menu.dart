2c63480c3287a30cce34f8bad37958c60ae8cc06
kang <zxcvb2935@kumoh.ce.kr>
2024-12-04 04:29:41 +0900

    공지사항, 문의하기, 로그인 화면 임시로 만들어 넣음

--- lib/src/menu.dart
+++ lib/src/menu.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import '../screens/login_screen.dart';
 import 'Info.dart';
 
 import '/SingleTone/font.dart';
+import 'InquriyScreen.dart';
+import 'NoticeListPage.dart';
 
 
 class MenuScreen extends StatefulWidget {
@@ -97,7 +100,12 @@ class _MenuScreen extends State<MenuScreen> {
               const SizedBox(height: 16),
               // Login Button
               OutlinedButton(
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LoginScreen()),
+                  );
+                },
                 style: OutlinedButton.styleFrom(
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(20),
@@ -117,13 +125,13 @@ class _MenuScreen extends State<MenuScreen> {
               // Menu Items
               _buildMenuItem(Icons.notifications_none_outlined, '공지사항',
                     () => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => InfoScreen()),
+                  MaterialPageRoute(builder: (context) => NoticeListPage()),
                 ),
               ),
               const Divider(height: 1),
               _buildMenuItem(Icons.chat_bubble_outline, '문의하기',
                     () => Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => InfoScreen()),
+                  MaterialPageRoute(builder: (context) => InquiryScreen()),
                 ),),
               const Divider(height: 1),
               _buildMenuItem(Icons.info_outline, '정보',
@@ -156,3 +164,4 @@ class _MenuScreen extends State<MenuScreen> {
     );
   }
 }
+
