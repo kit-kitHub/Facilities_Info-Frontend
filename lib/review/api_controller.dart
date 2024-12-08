95512ce3f395df8c1e2537c9233cab27de54f389
kang <zxcvb2935@kumoh.ce.kr>
2024-12-09 08:37:17 +0900

    이전으로 복귀

--- lib/review/api_controller.dart
+++ lib/review/api_controller.dart
@@ -6,16 +6,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiController {
   static const String baseUrl = 'http://3.34.105.70:8080/api';
+  static const String accessToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJGYWNpbGl0aWVzLW1hcC1zZXJ2aWNlIiwiaWF0IjoxNzMzMzE1MzIyLCJleHAiOjE3MzM0MDE3MjIsInN1YiI6InF3ZUBxd2UiLCJpZCI6Mywic25zSWRPckVtYWlsIjoicXdlQHF3ZSIsInByb3ZpZGVyIjoibG9jYWwifQ.fWYkLhjVzk_Yd64KE0BX5GEmeD7LuKVeiTA04pkhzd4';
+
   // Method to fetch the access token dynamically from SharedPreferences
 
-  static Future<String?> _getAccessToken() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    return prefs.getString('accessToken');  // Retrieve the stored access token
-  }
   static Future<Map<String, dynamic>> getFacilityWithReviews(int facilityId) async {
     final url = '$baseUrl/facility/$facilityId';
-    String? accessToken = await _getAccessToken();
-
     final response = await http.get(
       Uri.parse(url),
       headers: {
@@ -32,7 +28,6 @@ class ApiController {
 
   static Future<http.Response> updateFacilityWithImages(int facilityId, String name, String address, String description, List<File> images) async {
     final url = '$baseUrl/update/facility/info/$facilityId';
-    String? accessToken = await _getAccessToken();
     final request = http.MultipartRequest('PUT', Uri.parse(url));
 
     request.fields['name'] = name;
@@ -50,7 +45,6 @@ class ApiController {
 
   static Future<http.Response> addReview(Map<String, dynamic> reviewData) async {
     final url = '$baseUrl/review/add';
-    String? accessToken = await _getAccessToken();
     return await http.post(
       Uri.parse(url),
       headers: {
@@ -63,7 +57,6 @@ class ApiController {
 
   static Future<http.Response> deleteReview(int reviewId) async {
     final url = '$baseUrl/review/delete/$reviewId';
-    String? accessToken = await _getAccessToken();
     return await http.delete(
       Uri.parse(url),
       headers: {
@@ -74,7 +67,6 @@ class ApiController {
 
   static Future<http.Response> updateReview(int reviewId, Map<String, dynamic> reviewData) async {
     final url = '$baseUrl/review/update/$reviewId';
-    String? accessToken = await _getAccessToken();
     return await http.put(
       Uri.parse(url),
       headers: {
@@ -87,7 +79,6 @@ class ApiController {
 
   static Future<http.Response> toggleLike(int reviewId) async {
     final url = '$baseUrl/review/$reviewId/like';
-    String? accessToken = await _getAccessToken();
     return await http.put(
       Uri.parse(url),
       headers: {
