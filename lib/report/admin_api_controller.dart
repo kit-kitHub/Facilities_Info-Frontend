1d6d28adcec763b8ad10c59e0de5a50ca9a7890e
kang <zxcvb2935@kumoh.ce.kr>
2024-12-09 08:29:33 +0900

    운영자 토큰 관리(아직X)

--- lib/report/admin_api_controller.dart
+++ lib/report/admin_api_controller.dart
@@ -1,11 +1,17 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ApiController {
   static const String baseUrl = 'http://3.34.105.70:8080';
-  static const String _accessToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJGYWNpbGl0aWVzLW1hcC1zZXJ2aWNlIiwiaWF0IjoxNzMzMzE1MzIyLCJleHAiOjE3MzM0MDE3MjIsInN1YiI6InF3ZUBxd2UiLCJpZCI6Mywic25zSWRPckVtYWlsIjoicXdlQHF3ZSIsInByb3ZpZGVyIjoibG9jYWwifQ.fWYkLhjVzk_Yd64KE0BX5GEmeD7LuKVeiTA04pkhzd4';
+
+  static Future<String?> _getAccessToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('accessToken');  // Retrieve the stored access token
+  }
 
   Future<Map<String, dynamic>> blockUser(int userId) async {
+    String? _accessToken = await _getAccessToken();  // Fetch the token
     final response = await http.post(
       Uri.parse('$baseUrl/api/admin/block/$userId'),
       headers: {
@@ -20,6 +26,7 @@ class ApiController {
   }
 
   Future<void> unblockUser(int userId) async {
+    String? _accessToken = await _getAccessToken();  // Fetch the token
     final response = await http.post(
       Uri.parse('$baseUrl/api/admin/unblock/$userId'),
       headers: {
@@ -32,6 +39,7 @@ class ApiController {
   }
 
   Future<Map<String, dynamic>> getUserBlockRecord(int userId) async {
+    String? _accessToken = await _getAccessToken();  // Fetch the token
     final response = await http.get(
       Uri.parse('$baseUrl/api/admin/user-block/$userId'),
       headers: {
@@ -46,6 +54,7 @@ class ApiController {
   }
 
   Future<List<dynamic>> getAllBlockRecords() async {
+    String? _accessToken = await _getAccessToken();  // Fetch the token
     final response = await http.get(
       Uri.parse('$baseUrl/api/admin/blocks'),
       headers: {
