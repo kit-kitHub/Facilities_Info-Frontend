a9689dc8271c28c563637998599d37120b14b45f
kang <zxcvb2935@kumoh.ce.kr>
2024-12-09 07:09:46 +0900

    신고하기 화면 디자인 완료

--- lib/report/report_screen.dart
+++ lib/report/report_screen.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 import '/main.dart';
 import 'report_api_controller.dart';
 import '/SingleTone/fontSizeManager.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ReportScreen extends StatefulWidget {
   final String contentType;
   final int reviewId;
 
-  // 생성자에서 리뷰 ID와 콘텐츠 유형을 전달받음
   ReportScreen({required this.contentType, required this.reviewId});
 
   @override
@@ -15,40 +15,61 @@ class ReportScreen extends StatefulWidget {
 }
 
 class _ReportScreenState extends State<ReportScreen> {
-  final TextEditingController _reasonController = TextEditingController();
   String? _typeController;
+  final TextEditingController _contentController = TextEditingController();
   final fontSizeManager = FontSizeManager();
 
-  // 신고 생성 함수
+  // Method to get access token from SharedPreferences
+  Future<String?> _getAccessToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('accessToken');
+  }
+
+  // Method to create a report
   void _createReport() async {
-    // 필수 입력값 체크
-    if (_reasonController.text.isEmpty || _typeController == null) {
+    if ((_typeController?.isEmpty ?? true) || _contentController.text.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('이유와 신고 유형을 모두 선택해 주세요', style: TextStyle(fontSize: fontSizeManager.fontSize),)),
+        SnackBar(content: Text('신고 유형과 내용을 모두 입력해 주세요',
+            style: TextStyle(fontSize: fontSizeManager.fontSize))),
       );
       return;
     }
 
     final reportData = {
-      'reason': _reasonController.text,
       'type': _typeController,
+      'reason': _contentController.text,
     };
 
+    // Fetch access token before making the API call
+    String? accessToken = await _getAccessToken();
+    if (accessToken == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('로그인이 필요합니다.',
+            style: TextStyle(fontSize: fontSizeManager.fontSize))),
+      );
+      return;
+    }
+
     final response = await ApiController.createReport(
-      widget.contentType, // 전달된 콘텐츠 유형
-      widget.reviewId, // 전달된 리뷰 ID
+      widget.contentType,
+      widget.reviewId,
       reportData,
     );
 
     if (response.statusCode == 200) {
-      // 신고 성공 시 이전 화면으로 이동
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('신고되었습니다!', style: TextStyle(fontSize: fontSizeManager.fontSize),)));
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('신고되었습니다!',
+            style: TextStyle(fontSize: fontSizeManager.fontSize))),
+      );
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => HomeScreen()), // HomeScreen으로 이동
+        MaterialPageRoute(builder: (context) => HomeScreen()),
       );
     } else {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('오류로 인해 신고에 실패하였습니다', style: TextStyle(fontSize: fontSizeManager.fontSize),)));
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('오류로 인해 신고에 실패하였습니다',
+            style: TextStyle(fontSize: fontSizeManager.fontSize))),
+      );
     }
   }
 
@@ -56,46 +77,81 @@ class _ReportScreenState extends State<ReportScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('신고 하기'),
+        title: Text('신고 화면'),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        elevation: 0,
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              // 신고 이유 입력 (텍스트 필드 크기 동적으로 증가)
-              TextField(
-                controller: _reasonController,
-                decoration: InputDecoration(
-                  labelText: '이유',
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    DropdownButtonFormField<String>(
+                      value: _typeController,
+                      hint: Text('신고 유형을 선택해주세요'),
+                      items: ['신고 유형 1', '신고 유형 2', '신고 유형 3']
+                          .map((type) => DropdownMenuItem(
+                        value: type,
+                        child: Text(type),
+                      ))
+                          .toList(),
+                      onChanged: (value) {
+                        setState(() {
+                          _typeController = value;
+                        });
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    TextField(
+                      controller: _contentController,
+                      decoration: InputDecoration(
+                        labelText: '신고 내용',
+                        labelStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: fontSizeManager.fontSize,
+                        ),
+                        border: UnderlineInputBorder(),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.grey),
+                        ),
+                      ),
+                      maxLines: null,
+                      keyboardType: TextInputType.multiline,
+                      textInputAction: TextInputAction.newline,
+                    )
+                  ],
                 ),
-                maxLines: null, // 이 속성을 설정하여 입력 시 필드 크기가 늘어나도록 함
-                keyboardType: TextInputType.multiline, // 여러 줄 입력을 허용
-                minLines: 1, // 최소 1줄
               ),
-              // 신고 유형 입력
-              DropdownButtonFormField<String>(
-                value: _typeController,
-                hint: Text('신고 유형을 선택해주세요', style: TextStyle(fontSize: fontSizeManager.fontSize),),
-                items: ['신고 유형 1', '신고 유형 2', '신고 유형 3']
-                    .map((type) => DropdownMenuItem(
-                  value: type,
-                  child: Text(type),
-                ))
-                    .toList(),
-                onChanged: (value) {
-                  setState(() {
-                    _typeController = value;
-                  });
-                },
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: SizedBox(
+                width: double.infinity,
+                child: OutlinedButton(
+                  onPressed: _createReport,
+                  child: Text(
+                    '신고하기',
+                    style: TextStyle(
+                      fontSize: fontSizeManager.fontSize,
+                      color: Colors.red,
+                    ),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    side: BorderSide(color: Colors.red),
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
               ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: _createReport,
-                child: Text('신고 제출', style: TextStyle(fontSize: fontSizeManager.fontSize),),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
