a0bd87301fae945508405050233472925930ea65
kang <zxcvb2935@kumoh.ce.kr>
2024-12-10 04:22:05 +0900

    대략적인 주소 화면

--- lib/src/map/makerInfo.dart
+++ lib/src/map/makerInfo.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import '/report/report_screen.dart';
-import '/review/api_controller.dart';
-import '/review/facility_review_edit_screen.dart'; // ApiController 사용
+import '../addmapmaker/facility.dart';
+import '../addmapmaker/facility_controller.dart';
 import '/SingleTone/fontSizeManager.dart';
+import 'detailaddress.dart';
 
 class DraggableSheet extends StatefulWidget {
   final ScrollController scrollController;
@@ -28,58 +28,8 @@ class DraggableSheet extends StatefulWidget {
 }
 
 class _DraggableSheetState extends State<DraggableSheet> {
-  bool _isLiked = false;
-  late int _currentRating;
-  List<Map<String, dynamic>> _reviews = [];
-  final TextEditingController _reviewController = TextEditingController();
   final fontSizeManager = FontSizeManager();
 
-  @override
-  void initState() {
-    super.initState();
-    _currentRating = widget.rating;
-    _fetchFacilityAndReviews();
-  }
-
-  Future<void> _fetchFacilityAndReviews() async {
-    try {
-      final facilityData = await ApiController.getFacilityWithReviews(widget.facilityId);
-      setState(() {
-        _reviews = List<Map<String, dynamic>>.from(facilityData['reviews']);
-      });
-    } catch (e) {
-      print('Error fetching facility and reviews: $e');
-    }
-  }
-
-  Future<void> _submitReview(Map<String, dynamic> newReview) async {
-    try {
-      final response = await ApiController.addReview(newReview);
-      if (response.statusCode == 200 || response.statusCode == 201) {
-        setState(() {
-          _reviews.add(newReview);
-          _reviewController.clear();
-        });
-      } else {
-        print('Failed to add review');
-      }
-    } catch (e) {
-      print('Error submitting review: $e');
-    }
-  }
-
-  Future<void> _toggleLike(int reviewId) async {
-    try {
-      final response = await ApiController.toggleLike(reviewId);
-      if (response.statusCode == 200) {
-        setState(() {
-          _isLiked = !_isLiked;
-        });
-      }
-    } catch (e) {
-      print('Error toggling like: $e');
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -91,98 +41,36 @@ class _DraggableSheetState extends State<DraggableSheet> {
       child: ListView(
         controller: widget.scrollController,
         children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(widget.title, style: TextStyle(fontSize: fontSizeManager.fontSize + 4, fontWeight: FontWeight.bold)),
-                SizedBox(height: 8),
-                Text(widget.address, style: TextStyle(color: Colors.grey[700], fontSize: fontSizeManager.fontSize)),
-                SizedBox(height: 16),
-                Image.network(widget.imageUrl, fit: BoxFit.cover),
-                SizedBox(height: 16),
-                Text(widget.description, style: TextStyle(fontSize: fontSizeManager.fontSize)),
-                SizedBox(height: 16),
-
-                Row(
-                  children: List.generate(5, (index) {
-                    return Icon(
-                      index < _currentRating ? Icons.star : Icons.star_border,
-                      color: Colors.amber,
-                    );
-                  }),
-                ),
-                SizedBox(height: 16),
-
-                TextField(
-                  controller: _reviewController,
-                  readOnly: true,
-                  onTap: () async {
-                    final newReview = await Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => FacilityReviewEditScreen(),
-                        settings: RouteSettings(arguments: widget.facilityId),
-                      ),
-                    );
-
-                    if (newReview != null) {
-                      _submitReview(newReview); // 새로운 리뷰 저장
-                    }
-                  },
-                  decoration: InputDecoration(
-                    hintText: '리뷰 작성 또는 수정하기',
-                    border: OutlineInputBorder(),
+          GestureDetector(
+            onTap: () async{
+              Facility fac = await ApiService().getFacilityById(widget.facilityId);
+              FacilityDetailScreen(facility: fac);
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    widget.title,
+                    style: TextStyle(
+                        fontSize: fontSizeManager.fontSize + 4,
+                        fontWeight: FontWeight.bold),
                   ),
-                ),
-                SizedBox(height: 16),
-                Divider(),
-                ..._reviews.map((review) {
-                  return ListTile(
-                    leading: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        IconButton(
-                          onPressed: () {
-                            // 신고 버튼 클릭 시 해당 리뷰의 id와 콘텐츠 유형을 전달
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ReportScreen(
-                                  reviewId: review['id'], // 리뷰 ID를 전달
-                                  contentType: 'Review', // 콘텐츠 유형 'Review'
-                                ),
-                              ),
-                            );
-                          },
-                          icon: Icon(
-                            Icons.notification_important, // 사이렌 대신 경고 아이콘
-                            color: Colors.red,
-                            size: 30,
-                          ),
-                        ),
-                        SizedBox(width: 8), // 버튼과 아바타 간의 간격
-                        CircleAvatar(),
-                      ],
-                    ),
-                    title: Text(review['username'] ?? 'Unknown'),
-                    subtitle: Text(review['content']),
-                    trailing: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(review['date']),
-                        IconButton(
-                          icon: Icon(_isLiked ? Icons.thumb_up : Icons.thumb_up_off_alt),
-                          onPressed: () => _toggleLike(review['id']),
-                        ),
-                      ],
-                    ),
-                  );
-                }).toList(),
-                if (_reviews.isEmpty)
-                  Center(child: Text('리뷰가 없습니다.', style: TextStyle(fontSize: fontSizeManager.fontSize),)),
-              ],
+                  SizedBox(height: 8),
+                  Text(
+                    widget.address,
+                    style: TextStyle(
+                        color: Colors.grey[700], fontSize: fontSizeManager.fontSize),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    widget.description,
+                    style: TextStyle(fontSize: fontSizeManager.fontSize),
+                  ),
+                  SizedBox(height: 16),
+                ],
+              ),
             ),
           ),
         ],
